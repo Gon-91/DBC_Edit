959b762d669be8696fc6f9892a0f44499f565951
HG Oh <hk1069@naver.com>
2026-01-29 15:15:52 +0900

    Update .gitignore to include additional patterns and remove obsolete entries

--- src/viewmodels/__init__.py
+++ src/viewmodels/__init__.py
@@ -1,4 +1,4 @@
 from .central.central_view_model import CentralViewModel
 from .explorer.explorer_view_model import ExplorerViewModel
 
-__all__ = ["CentralViewModel","SignalTableModel","ExplorerViewModel"]
+__all__ = ["CentralViewModel", "ExplorerViewModel"]
